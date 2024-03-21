77b99728325832c023eb7d9b166d8146e5b00925
syzygy1019 <104407653+syzygy1019@users.noreply.github.com>
2024-03-21 14:48:01 +0800

    version222

---
+++ note.js
@@ -0,0 +1,82 @@
+
+alert('Hello world!!');
+alert('How are you?');
+alert('I am fine.Thank you.')
+
+var x=1;
+var y=x+519;
+alert (y);
+
+var z='I am kid ';
+var temp=z+'from k-town'
+alert(temp+' who like photography nice to meet you.');
+
+function alertx(){
+var x='I do not know what to say now.';
+alert(x);
+}
+alertx();
+alertx();
+
+alert('How about let us play a game together?');
+ var n = 2048;
+//parseInt(Math.random()*(b-a+1))+a;
+while(true) {
+ var a = Number(prompt("Guess a number!"));
+ if(a>n){
+  alert("Too bigggg!!");
+ }else if(a<n) {
+  alert("Too smallll!!");
+ }else {
+  alert("Yah!!! You get it.");
+  break;
+ }
+}
+
+function over(){document.querySelector("#aaa").innerHTML="INTRO-What is it?";
+		document.querySelector("#aaa").style.color="#719BA5";}
+		
+function unover(){document.querySelector("#aaa").innerHTML="NO.1-1";
+		document.querySelector("#aaa").style.color="black";}
+
+        function over1(){document.querySelector("#ccc").innerHTML="INTRO-How to preserve the film?";
+		document.querySelector("#ccc").style.color="#719BA5";}
+		
+function unover1(){document.querySelector("#ccc").innerHTML="NO.1-2";
+		document.querySelector("#ccc").style.color="black";}
+
+		function over2(){document.querySelector("#ddd").innerHTML="Compare-NEGATIVE X REVERSAL";
+		document.querySelector("#ddd").style.color="#719BA5";}
+		
+function unover2(){document.querySelector("#ddd").innerHTML="NO.2-1";
+		document.querySelector("#ddd").style.color="black";}
+
+		function over3(){document.querySelector("#eee").innerHTML="Compare-BLACk & WHITE FILM X COLOR  FILM";
+		document.querySelector("#eee").style.color="#719BA5";}
+		
+function unover3(){document.querySelector("#eee").innerHTML="NO.2-2";
+		document.querySelector("#eee").style.color="black";}
+
+		function over4(){document.querySelector("#fff").innerHTML="Compare-MOTION  PICTURE X COMMON";
+		document.querySelector("#fff").style.color="#719BA5";}
+		
+function unover4(){document.querySelector("#fff").innerHTML="NO.2-3";
+		document.querySelector("#fff").style.color="black";}
+
+		function over5(){document.querySelector("#ggg").innerHTML="Compare-PROFESSIONAL X COMMON";
+		document.querySelector("#ggg").style.color="#719BA5";}
+		
+function unover5(){document.querySelector("#ggg").innerHTML="NO.2-4";
+		document.querySelector("#ggg").style.color="black";}
+
+		function over6(){document.querySelector("#hhh").innerHTML="Feature-Kodak & Fuji & AGFA";
+		document.querySelector("#hhh").style.color="#719BA5";}
+		
+function unover6(){document.querySelector("#hhh").innerHTML="NO.3-1";
+		document.querySelector("#hhh").style.color="black";}
+
+
+ 
+
+ 
+
